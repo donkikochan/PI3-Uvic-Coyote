516ebc62e5b25e31bd08b9340540cc37dd087171
Enric Vergara <donkikochan@gmail.com>
2023-03-14 18:57:46 +0100

    afegit la scene login vr

---
+++ Assets/LoginUIManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginUIManager : MonoBehaviour
+{
+    public GameObject ConnectOptionsPanel;
+    public GameObject ConnectWithNamePanel;
+
+    #region Unity Methods
+    void Start()
+    {
+        ConnectOptionsPanel.SetActive(true);
+        ConnectWithNamePanel.SetActive(false);
+    }
+    #endregion
+}
